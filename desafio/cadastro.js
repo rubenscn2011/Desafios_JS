61989b4dc95967d3de0bbe58284dfc3fdc014651
rubenscn2011 <rubenscn2011@gmail.com>
2026-06-01 20:33:10 -0300

    Desafio 01

---
+++ desafio/cadastro.js
@@ -0,0 +1,31 @@
+//Desafio 01 - Cadastro de Pessoa
+
+/*
+ Crie um objeto chamado pessoa contendo:
+ - nome
+ - idade
+ - cidade
+ Após a criação:
+ - Altere a idade
+ - Adicione uma profissão
+ - Remova a cidade
+ No fim, exiba o objeto final.
+*/
+
+const pessoa= {
+    nome: "Rubens",
+    idade: "30",
+    cidade: "Capanema"
+
+}
+   console.log(pessoa);
+   console.log(pessoa.nome);
+   console.log(pessoa.idade);
+   pessoa.idade = 18;
+   
+   pessoa.profissão ="Técnico em Desenvolvimento de Sistemas"
+   delete pessoa.cidade;
+
+
+   
+
