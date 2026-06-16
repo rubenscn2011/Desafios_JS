d9de03defe388ef2c39fca52dd0b0a0205de6b07
rubenscn2011 <rubenscn2011@gmail.com>
2026-06-15 21:51:49 -0300

    Desafio 4

---
+++ Desafio4/loja.js
@@ -0,0 +1,17 @@
+/*
+DESAFIO 04 - LOJA TECH
+
+Uma loja de informática possui vários produtos cadastrados.
+
+Crie um array de objetos contendo:
+- nome
+- preco
+
+Utilize uma estrutura de repetição para mostrar apenas
+ os produtos que custam mais de R$ 100,00.
+
+ Exiba o nome dos produtos encontrados.
+ */
+const nome = ["mouse", "teclado", "notebook"];
+
+const preco= ["100"];
