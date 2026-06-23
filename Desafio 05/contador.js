a8dc0361831b2b7db1a1bd174683871d588381e4
rubenscn2011 <rubenscn2011@gmail.com>
2026-06-22 21:50:16 -0300

    contador concluido

---
+++ Desafio 05/contador.js
@@ -0,0 +1,26 @@
+/*
+DESAFIO 05 - CONTADOR INTELIGENTE
+
+Crie uma função chamada contador() que receba:
+- valor inicial
+- valor final
+
+A função deve exibir todos os números entre os valores informados.
+
+Após concluir, cire uma segunda versão capaz de realizar a contagem regressiva.
+*/
+
+function contador(inicial, final){
+    for(let i = inicial; i <= final; i++){
+        console.log(i);
+    }
+}
+function contadorRegressivo(inicial, final) {
+        console.log("Contador Decrescente");
+        for (let i = inicial; i >= final; i--) {
+        console.log(i);
+        }
+    }           
+
+contador(0, 10);
+contadorRegressivo(10, 0);
